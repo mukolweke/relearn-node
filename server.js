26921e45a3ce1c356d089ea7692eaf34261e5e96
Michael Olukaka <molukaka@cytonn.com>
2020-03-29 17:30:43 +0300

    master server

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ const hostname = '127.0.0.1';
 
 const port = 3000;
 
+//create a server object:
 const server = http.createServer(
     (req, res) => {  
         res.statusCode = 200  
-        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Type', 'text/html');
+        //write a response to the client
         res.write("The date and time are currently: " + dateTime.myDateTime()); 
+        //end the response
         res.end();
     }
-).listen(
+)
+
+
+//the server object listens on port 8080
+server.listen(
     port, 
     hostname, 
     () => {  
         console.log(`Server running at http://${hostname}:${port}/`);
     }
 );
-
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.end('Hello World!');
-//   }).listen(8080);
